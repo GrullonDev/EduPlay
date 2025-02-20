ac8bbbe6e33a5a845a3edba11a53d758aae0c56f
GrullonDev <prosystem155@gmail.com>
2025-02-19 21:18:26 -0600

    Refactor AppRouter to improve route handling and remove unused methods

--- lib/utils/routes/router_switch.dart
+++ lib/utils/routes/router_switch.dart
@@ -9,33 +9,34 @@ import 'package:edu_play/utils/routes/router_paths.dart';
 
 class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
-    Widget? page;
+    Widget page;
     String name = settings.name ?? '';
 
     switch (name) {
       case RouterPaths.root:
         page = const LoginPage();
+        break;
       case RouterPaths.register:
         page = const RegisterPage();
+        break;
       case RouterPaths.registerChild:
         page = const RegisterChild();
+        break;
       case RouterPaths.menu:
         final userName = settings.arguments as String?;
-        print(userName);
         page = MenuPage(username: userName);
+        break;
       case RouterPaths.mathAdventure:
         final userName = settings.arguments as String?;
-        print(userName);
         page = MathAdventurePage(userName: userName);
-
+        break;
       default:
-        return MaterialPageRoute(
-          builder: (_) => const Scaffold(
-            body: Center(
-              child: Text('Page not found'),
-            ),
+        page = const Scaffold(
+          body: Center(
+            child: Text('Page not found'),
           ),
         );
+        break;
     }
 
     return _getPageRoute(name, page, settings.arguments);
@@ -47,44 +48,6 @@ class AppRouter {
     Object? args,
   ) {
     // sl<GoogleAnalyticsService>().setScreenName(routeName);
-    return MaterialPageRoute(
-      settings: RouteSettings(
-        name: routeName,
-        arguments: args,
-      ),
-      builder: (_) => viewToShow,
-    );
-  }
-
-  static PageRoute _getPageRouteMap(
-    String routeName,
-    dynamic viewToShow,
-    Object? args,
-  ) {
-    // sl<GoogleAnalytics>().setScreenName(routeName);
-    return MaterialPageRoute<Map<String, dynamic>>(
-      settings: RouteSettings(
-        name: routeName,
-        arguments: args,
-      ),
-      builder: (_) => viewToShow,
-    );
-  }
-
-  // Valida use because it call dispose navigation to a new page
-  static PageRoute _fadeRoute(
-    String routeName,
-    dynamic viewToShow,
-    Object? args,
-  ) {
-    /* sl<GoogleAnalytics>().setScreenName(routeName);
-    return FadeTransitionRoute(
-      settings: RouteSettings(
-        name: routeName,
-        arguments: args,
-      ),
-      page: viewToShow,
-    ); */
     return MaterialPageRoute(
       settings: RouteSettings(
         name: routeName,
