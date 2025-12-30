251d3d5bd26d6e053df0827bb306f6d1f6d04985
Jorge Grullon - TomzaGroup <jorge.grullon@tomzagroup.com>
2025-12-30 16:19:44 -0600

    feat: Initialize project with Firebase setup, core assets, and web hosting configuration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:edu_play/utils/injection_container.dart' as di;
 import 'package:edu_play/utils/my_app.dart';
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-// import 'firebase_options.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   GoogleFonts.config.allowRuntimeFetching = true;
   di.init();
   runApp(const MyApp());
