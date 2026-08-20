e60529b682a57e52222c3aa6bc883ce8d9f34659
Jorge-Tropigas <jorge.grullon@tomzagroup.com>
2026-08-20 16:42:28 -0600

    feat: add admin classes management view

--- lib/features/admin/domain/repositories/admin_dashboard_repository.dart
+++ lib/features/admin/domain/repositories/admin_dashboard_repository.dart
@@ -1,10 +1,8 @@
 // Project imports:
 import 'package:edu_play/features/admin/domain/entities/platform_stats.dart';
+import 'package:edu_play/features/teacher_dashboard/domain/entities/teacher_class.dart';
 
 abstract class AdminDashboardRepository {
   Future<PlatformStats?> loadStats();
-  Future<bool> setAdminRoleByEmail({
-    required String email,
-    required bool granting,
-  });
+  Future<List<TeacherClass>> listAllClasses();
 }
